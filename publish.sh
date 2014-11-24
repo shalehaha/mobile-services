1f0b6b6060841bed1567d47e03c671702a0b598d
jgeng <jgeng@adobe.com>
2014-11-24 15:48:54 -0700

    Release 4.3.1

---
+++ publish.sh
@@ -0,0 +1,16 @@
+# ./publish.sh version
+echo "Version is $1"
+
+echo "Update the Version number in podspec"
+sed -i "" "s/\\(s.version[[:space:]]*=[[:space:]]*\"\\).*\\(\"\\)/\\1$1\\2/g" AdobeMobileSDK.podspec
+sed -i "" "s/\\(\"v\).*\\(-cocoapod\"\\)/\\1$1\\2/g" AdobeMobileSDK.podspec
+
+echo "Rename AdobeMobileLibrary"
+
+echo ""
+git add *
+git commit -m "Release $1"
+git push origin cocoapod
+
+git tag v$1-cocoapod
+git push origin v$1-cocoapod
